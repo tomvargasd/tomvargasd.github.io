3a09a401d5a32346057c88e14964deeabc8bfcc6
tomvargas <tomvargasd@gmail.com>
2022-07-23 13:06:38 -0500

    change my photo

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -172,7 +172,7 @@ const About = () => {
               className="img"
               src="../../images/me.jpg"
               width={500}
-              quality={95}
+              quality={90}
               formats={['AUTO', 'WEBP', 'AVIF']}
               alt="Headshot"
             />
